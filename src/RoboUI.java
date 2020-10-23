98780198755ad368f3d54d113d9a5079d0a2574b
sanvi1855544 <sanvi11929@gmail.com>
2020-10-23 08:20:53 -0700

    Added hangbot

---
+++ src/RoboUI.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class RoboUI extends JPanel{
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        int w = getWidth();
+        int h = getHeight();
+
+        //g.setColor(Color.RED);
+        //g.drawRect(w/4, h/4,w/2,h/2);
+
+        Toolkit t=Toolkit.getDefaultToolkit();
+        Image i = t.getImage("C:/Users/sanvi/IdeaProjects/Hangman/out/production/Hangman/images.png");
+        g.drawImage(i, 0, 0,this);
+
+    }
+}
