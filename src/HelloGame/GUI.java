eff031b264b6127d157c8a0b7493b3e86db18897
YasaswiS <yasaswisingamneni2004@gmail.com>
2020-10-29 08:26:35 -0700

    adding HelloGame GUI

---
+++ src/HelloGame/GUI.java
@@ -0,0 +1,21 @@
+package HelloGame;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GUI extends JPanel {
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        int w = getWidth();
+        int h = getHeight();
+
+        //g.setColor(Color.RED);
+        //g.drawRect(w/4, h/4,w/2,h/2);
+
+        Toolkit t=Toolkit.getDefaultToolkit();
+        Image i = t.getImage("images.png");
+        g.drawImage(i, 0, 0,this);
+
+    }
+}
+
