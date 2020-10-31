cec0e94fe84d679eb498bf9783ccb1eb316c7146
Nakul Nandhakumar <nakul@nandhakumar.net>
2020-10-30 22:43:04 -0700

    README updates and main menu updates

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 import BattleshipGame.*;
-import HangbotGame.HangbotGUI;
-import TrioCalculator.*;
 import HelloGame.*;
+import SanviSaraCalculator.CalculatorUI;
+import SanviSaraCalculator.ConsoleBasedUI;
+import TrioCalculator.*;
 import HangbotGame.*;
 
 import javax.swing.*;
@@ -9,9 +10,13 @@ import javax.swing.*;
 public class Main extends JFrame {
     private JButton Battleship;
     private JPanel panel1;
-    private JButton HelloGame;
     private JButton Hangbot;
-    private JButton Calculator;
+    private JButton TrioCalculator;
+    private JButton Sara_Sanvi_CalcGUI;
+    private JButton RoboPrintButton;
+    private JButton HelloGameButton;
+    private JButton Sara_Sanvi_CalcConsole;
+    private JLabel mainLabel;
 
     public static void main(String[] args) {
         Main main = new Main();
@@ -26,11 +31,20 @@ public class Main extends JFrame {
         Battleship.addActionListener(e -> {
             BattleshipGUI.main(null);
         });
-        Calculator.addActionListener(e -> {
+        TrioCalculator.addActionListener(e -> {
             CalculatorContrl.main(null);
         });
-        HelloGame.addActionListener(e -> {
+        RoboPrintButton.addActionListener(e -> {
             RoboPrint.main(null);
+        });
+        HelloGameButton.addActionListener(e -> {
+            HelloGame.main(null);
+        });
+        Sara_Sanvi_CalcConsole.addActionListener(e -> {
+            CalculatorUI.main(null);
+        });
+        Sara_Sanvi_CalcGUI.addActionListener(e -> {
+            ConsoleBasedUI.main(null);
         });
         Hangbot.addActionListener(e -> {
             HangbotStart.main(null);
